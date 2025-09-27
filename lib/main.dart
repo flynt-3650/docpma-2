162d7e6c26c51b43539b012a62a0df17c3fec406
flynt-3650 <repilovi@gmail.com>
2025-09-27 10:23:05 +0300

    docs: add documentation comments and enable Material 3 design system

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
 
+/// Entry point of the student information application
 void main() {
   runApp(const MyApp());
 }
 
+/// Main application widget that sets up the MaterialApp
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Студенческая информация',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true, // Enable Material 3 design system
       ),
       home: const MyHomePage(title: 'Информация о студенте'),
+      debugShowCheckedModeBanner: false, // Hide debug banner in debug mode
     );
   }
 }
 
+/// Home page widget that displays student information
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
+  /// The title displayed in the app bar
   final String title;
 
   @override
