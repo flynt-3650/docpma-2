1f20309ecefcef5b6a97e7152cdd20e737e839e9
flynt-3650 <repilovi@gmail.com>
2025-10-03 23:10:32 +0300

    Добавлена логика счётчика кликов

--- lib/main.dart
+++ lib/main.dart
@@ -21,19 +21,30 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
-  void _incrementCounter() {}
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Center(
         child: Column(
@@ -59,9 +70,12 @@ class MyHomePage extends StatelessWidget {
               children: <Widget>[
                 const Text('Счётчик нажатий:', style: TextStyle(fontSize: 18)),
                 const SizedBox(width: 10),
-                const Text(
-                  '0',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                Text(
+                  '$_counter',
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ],
             ),
