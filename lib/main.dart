05cdaaee95861b1d170357cc6d1987804ee4706f
flynt-3650 <repilovi@gmail.com>
2025-10-06 22:13:09 +0300

    simplified list

--- lib/main.dart
+++ lib/main.dart
@@ -9,54 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Простейший список',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SimpleListScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class SimpleListScreen extends StatelessWidget {
-  const SimpleListScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // Данные для списка в виде String
-    final List<String> items = [
-      'Первый элемент списка',
-      'Второй элемент списка',
-      'Третий элемент списка',
-      'Четвертый элемент списка',
-      'Пятый элемент списка',
-    ];
+    final items = List.generate(10, (index) => 'Item ${index + 1}');
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Простейший список'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        // Контейнер в виде Column с элементами списка
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Элементы списка в виде Text с данными String
-            for (String item in items)
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: Text(
-                  item,
-                  style: const TextStyle(fontSize: 16),
-                ),
-              ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: items.map((item) => Text(item)).toList(),
       ),
     );
   }
